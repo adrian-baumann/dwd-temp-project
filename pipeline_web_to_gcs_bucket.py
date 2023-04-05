9f38a3a8beaee20bd1504889338da734af6a836e
Adrian <adrian.moritz@mail.de>
2023-04-05 17:53:51 +0200

    still debugging

--- pipeline_web_to_gcs_bucket.py
+++ pipeline_web_to_gcs_bucket.py
@@ -5,7 +5,8 @@ import pandas as pd
 import pyarrow as pa
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket, GcpCredentials
-from prefect_gcp import bigquery
+
+# from prefect_gcp import bigquery
 from google.cloud.bigquery import HivePartitioningOptions
 from random import randint
 from prefect.tasks import task_input_hash
